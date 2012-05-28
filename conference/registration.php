1b88e039f56001ce437f42e934f9bc7f29c76bee
daifu ye <daifu.ye@gmail.com>
2012-05-28 10:55:42 -0700

    added registration page

---
+++ conference/registration.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width" />
+
+	<title>VEMEC Conference</title>
+  
+	<!-- Included CSS Files -->
+   <link rel="stylesheet" href="../stylesheets/foundation.css">
+   <link rel="stylesheet" href="../stylesheets/flickrbomb.css">
+   <link rel="stylesheet" href="../stylesheets/app.css">
+
+   <!--Google Web Font-->
+   <link href='http://fonts.googleapis.com/css?family=Cambo' rel='stylesheet' type='text/css'>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="../stylesheets/ie.css">
+	<![endif]-->
+
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+   <script src="../javascripts/foundation.js"></script>
+</head>
+<body>
+
+	<!-- container -->
+   <div class="container">
+
+      <div class="row">
+         <div class="twelve columns" style="margin: 10px 0 10px;">
+            <a href="http://www.publichealth.va.gov">
+               <img src="../images/VA_Banner.png" alt="United States Department of Veterans Affairs" title="United States Department of Veterans Affairs" />
+            </a>
+         </div>
+      </div>
+
+      <div class="row subtitle">
+         <div class="twelve columns">
+            <h4 style="text-align:center;font-family: 'Cambo', serif;">Advancing &amp; Redefining Community for Emergency Management</h4>
+            <!-- img src="../images/header_shadow.png" width="100%" class="hide-on-phones half-opacity" id="header-shadow" alt=""/-->
+            <hr>
+         </div>
+      </div>
+
+      <div class="row">
+         <div class="three columns">
+            <dl class="nice tabs vertical hide-on-phones">
+               <dd>
+               <a href="/conference/about.html">About</a>
+               </dd>
+               <dd>
+               <a href="/conference/registration.php" class="active">Registration</a>
+               </dd>
+               <dd>
+               <a href="#schedule">Schedule</a>
+               </dd>
+               <dd>
+               <a href="#travel">Travel</a>
+               </dd>
+               <dd>
+               <a href="#faq">FAQ</a>
+               </dd>
+            </dl>
+            <!-- Save it on your calendar -->
+            <div class="save-the-date hide-on-phones">
+               <a href="#save-the-date" class="white medium button radius" style="padding-left: 30px;vertical-align:bottom">
+                  <img src="../images/Calendar.png" alt="calendar" style="float:left;" />
+                  Save the date on your calendar
+               </a>
+            </div>
+         </div>
+         <!-- End With Side bar for Non-mobile -->
+
+         <div class="nine columns">
+            <!-- Insert the banner for the website -->
+            <img src="../images/conference/ARC_Olive_OrangeLetter_SemiFinalist.png" alt="" width="100%" height="100%"/>
+         </div>
+
+         <div class="nine columns conf-content">
+            <!-- Insert contents -->
+            <!-- End with contents -->
+      </div>
+      
+      <!-- Start with Side bar for mobile -->
+      <div class="row">
+         <div class="twelve columns">
+            <dl class="nice tabs mobile show-on-phones">
+               <dd>
+                  <a href="#about" class="active">About</a>
+               </dd>
+               <dd>
+                  <a href="#registration">Registration</a>
+               </dd>
+               <dd>
+                  <a href="#schedule">Schedule</a>
+               </dd>
+               <dd>
+                  <a href="#travel">Travel</a>
+               </dd>
+               <dd>
+                  <a href="#faq">FAQ</a>
+               </dd>
+            </dl>
+         </div>
+      </div>
+      <!-- End With Side bar for mobile -->
+      
+      <!-- Das Footer -->
+      <footer class="row hide-on-phones">
+         <div class="twelve columns">
+            <a href="#">VA Home</a> |
+            <a href="#">Privacy</a> |
+            <a href="#">FOIA</a> |
+            <a href="#">Regulations</a> |
+            <a href="#">Web Policies</a>
+         </div>
+      	<div class="twelve columns"></div>
+      </footer>
+      
+	</div>
+	<!-- container -->
+
+
+
+
+	<!-- Included JS Files -->
+  <script src="../javascripts/jquery.min.js"></script>
+  <script src="../javascripts/modernizr.foundation.js"></script>
+  <script src="../javascripts/foundation.js"></script>
+  <script src="../javascripts/app.js"></script>
+  <script src="../javascripts/jquery.flickrbomb.min.js"></script>
+  <script src="../javascripts/app.js"></script>
+</body>
+</html>
